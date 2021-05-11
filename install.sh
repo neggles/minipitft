ca71400895fe3af9ae333e5ad329ddd489d85b68
neg2led <4232981+neg2led@users.noreply.github.com>
2021-05-11 12:38:12 +1000

    tweaking scripts to work

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ DRV_NAME="minipitft"
 DRV_VERSION="0.1.0-0"
 
 # make git archive the repo to the right spot
+mkdir -p "/usr/src/${DRV_NAME}-${DRV_VERSION}"
 git archive HEAD | tar -x -C "/usr/src/${DRV_NAME}-${DRV_VERSION}"
 
 # run dkms
